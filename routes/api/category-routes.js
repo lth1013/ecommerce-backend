9f0689168e01cdccaf4df152b8e7b803366c711d
unknown <lukeharriman1013@gmail.com>
2023-08-08 23:25:06 -0700

    debug

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
     // include: [Product],
     include: {
       model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      attributes: ["id", "product_name", "price", "category_id"],
     },
   })
     .then((dbCategoryData) => res.json(dbCategoryData))
@@ -29,7 +29,7 @@ router.get("/:id", (req, res) => {
     },
     include: {
       model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      attributes: ["id", "product_name", "price", "category_id"],
     },
   }).then((dbCategoryData) => {
     if (!dbCategoryData) {
